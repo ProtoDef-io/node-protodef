cf217f72176a8e949345176e7254b402cfc2c48f
Romain Beaumont <romain.rom1@gmail.com>
2015-12-02 18:06:14 +0100

    structures

--- src/datatypes/structures.js
+++ src/datatypes/structures.js
@@ -1,9 +1,9 @@
 var { getField, tryDoc } = require("../utils");
 
 module.exports = {
-  'array': [readArray, writeArray, sizeOfArray],
-  'count': [readCount, writeCount, sizeOfCount],
-  'container': [readContainer, writeContainer, sizeOfContainer]
+  'array': [readArray, writeArray],
+  'count': [readCount, writeCount],
+  'container': [readContainer, writeContainer]
 };
 
 
@@ -29,22 +29,13 @@ async function readArray(read, {type,count,countType,countTypeArgs}, rootNode) {
   return results;
 }
 
-function writeArray(value, buffer, offset, {type,count,countType,countTypeArgs}, rootNode) {
+function writeArray(value, write, {type,count,countType,countTypeArgs}, rootNode) {
   if (typeof count === "undefined" && typeof countType !== "undefined")
-    offset= tryDoc(() => this.write(value.length, buffer, offset, { type: countType, typeArgs: countTypeArgs }, rootNode),"$count");
+    tryDoc(() => this.write(value.length, write, { type: countType, typeArgs: countTypeArgs }, rootNode),"$count");
   else if (typeof count === "undefined") { // Broken schema, should probably error out
   }
-  return value.reduce((offset,v,index) =>tryDoc(() => this.write(v, buffer, offset, type, rootNode),index),offset);
+  value.map((v,index) =>tryDoc(() => this.write(v, write, type, rootNode),index));
 }
-
-function sizeOfArray(value, {type,count,countType,countTypeArgs}, rootNode) {
-  var size = 0;
-  if (typeof count === "undefined" &&  typeof countType !== "undefined")
-    size=tryDoc(() => this.sizeOf(value.length, { type: countType, typeArgs: countTypeArgs }, rootNode),"$count");
-
-  return value.reduce((size,v,index) =>tryDoc(() => size+this.sizeOf(v, type, rootNode), index),size);
-}
-
 
 async function readContainer(read, typeArgs, context) {
   var values=await typeArgs.reduce(async (p,{type,name,anon}) =>
@@ -63,33 +54,20 @@ async function readContainer(read, typeArgs, context) {
   return values;
 }
 
-function writeContainer(value, buffer, offset, typeArgs, context) {
+function writeContainer(value, write, typeArgs, context) {
   value[".."] = context;
-  offset=typeArgs.reduce((offset,{type,name,anon}) =>
-    tryDoc(() => this.write(anon ? value : value[name], buffer, offset, type, value),name ?  name : "unknown"),offset);
+  typeArgs.map(({type,name,anon}) =>
+    tryDoc(() => this.write(anon ? value : value[name], write, type, value),name ?  name : "unknown"));
   delete value[".."];
-  return offset;
-}
-
-function sizeOfContainer(value, typeArgs, context) {
-  value[".."] = context;
-  var size = typeArgs.reduce((size,{type,name,anon}) =>
-    size + tryDoc(() => this.sizeOf(anon ? value : value[name], type, value), name ? name : "unknown"),0);
-  delete value[".."];
-  return size;
 }
 
 function readCount(read, {type}, rootNode) {
   return this.read(read, type, rootNode);
 }
 
-function writeCount(value, buffer, offset, {countFor,type}, rootNode) {
+function writeCount(value, write, {countFor,type}, rootNode) {
   // Actually gets the required field, and writes its length. Value is unused.
   // TODO : a bit hackityhack.
-  return this.write(getField(countFor, rootNode).length, buffer, offset, type, rootNode);
+  return this.write(getField(countFor, rootNode).length, write, type, rootNode);
 }
 
-function sizeOfCount(value, {countFor,type}, rootNode) {
-  // TODO : should I use value or getField().length ?
-  return this.sizeOf(getField(countFor, rootNode).length, type, rootNode);
-}
