93ec4f7835cd346d1246ebb5ed49c049a6aa9c58
Karang <arthur.hennequin@free.fr>
2020-05-11 21:46:27 +0200

    Refactor compiler

--- examples/compiled.js
+++ examples/compiled.js
@@ -2,9 +2,9 @@ const ProtoDef = require('protodef').ProtoDef
 const Serializer = require('protodef').Serializer
 const Parser = require('protodef').Parser
 const { performance } = require('perf_hooks')
-const { compile, optimize, generateRead } = require('protodef').Compiler
+const { ReadCompiler, optimize } = require('protodef').Compiler
 
-const exampleProtocol = require('./example_protocol.json') 
+const exampleProtocol = require('./example_protocol.json')
 const mainType = 'packet'
 const packetData = {
   name: 'entity_look',
@@ -16,7 +16,7 @@ const packetData = {
   }
 }
 
-/*const exampleProtocol = {
+/* const exampleProtocol = {
   "position": [
     "bitfield",
     [
@@ -43,11 +43,12 @@ const packetData = {
   x: 1024,
   y: 256,
   z: -1337
-}*/
+} */
 
-let code = generateRead(exampleProtocol, mainType)
+let compiler = new ReadCompiler()
+let code = compiler.generate(exampleProtocol)
 console.log(code)
-const test = compile(code)
+const test = compiler.compile(code)
 
 const proto = new ProtoDef()
 proto.addTypes(exampleProtocol)
@@ -56,39 +57,39 @@ const parser = new Parser(proto, mainType)
 
 const buffer = serializer.createPacketBuffer(packetData)
 
-console.log(JSON.stringify(test(buffer, 0), null, 2))
+console.log(JSON.stringify(test[mainType](buffer, 0), null, 2))
 console.log(JSON.stringify(parser.parsePacketBuffer(buffer), null, 2))
 
 const nbTests = 1000000
-console.log('Running '+nbTests+' tests')
+console.log('Running ' + nbTests + ' tests')
 
 let start, time, ps
 
 start = performance.now()
-for (let i=0 ; i<nbTests ; i++) {
-  const data = test(buffer, 0)
+for (let i = 0; i < nbTests; i++) {
+  test[mainType](buffer, 0)
 }
 time = performance.now() - start
 ps = nbTests / time
-console.log('read compiled: '+time.toFixed(2)+' ms ('+ps.toFixed(2)+'k packet/s)')
+console.log('read compiled: ' + time.toFixed(2) + ' ms (' + ps.toFixed(2) + 'k packet/s)')
 
 start = performance.now()
-for (let i=0 ; i<nbTests ; i++) {
-  const data = parser.parsePacketBuffer(buffer)
+for (let i = 0; i < nbTests; i++) {
+  parser.parsePacketBuffer(buffer)
 }
 time = performance.now() - start
 ps = nbTests / time
-console.log('parser: '+time.toFixed(2)+' ms ('+ps.toFixed(2)+'k packet/s)')
+console.log('parser: ' + time.toFixed(2) + ' ms (' + ps.toFixed(2) + 'k packet/s)')
 
 // Closure optimized:
 code = optimize(code, (code) => {
   // console.log(code)
-  const test2 = compile(code)
+  const test2 = compiler.compile(code)
   start = performance.now()
-  for (let i=0 ; i<nbTests ; i++) {
-    const data = test2(buffer, 0)
+  for (let i = 0; i < nbTests; i++) {
+    test2[mainType](buffer, 0)
   }
   time = performance.now() - start
   ps = nbTests / time
-  console.log('read compiled (+closure): '+time.toFixed(2)+' ms ('+ps.toFixed(2)+'k packet/s)')
+  console.log('read compiled (+closure): ' + time.toFixed(2) + ' ms (' + ps.toFixed(2) + 'k packet/s)')
 })
--- src/compiler.js
+++ src/compiler.js
@@ -3,174 +3,230 @@ const ClosureCompiler = require('google-closure-compiler').jsCompiler
 const numeric = require('./datatypes/numeric')
 const utils = require('./datatypes/utils')
 
-function indent(code, indent='  ') {
+function indent (code, indent = '  ') {
   return code.split('\n').map((line) => indent + line).join('\n')
 }
 
-/**
- * Compile a json protodef to js and return the code of the main deserialize function
- * @param {*} types 
- * @param {*} mainType 
- */
-function generateRead(types, mainType) {
-  const primitiveTypes = {}
-  for (const key in numeric) {
-    primitiveTypes[key] = [`numeric.${key}[0]`]
-  }
-  for (const key in utils) {
-    primitiveTypes[key] = [`utils.${key}[0]`]
-  }
-
-  const parameterizableTypes = {
-    // conditional
-    'switch': (struct) => {
-      const compare = struct.compareTo ? struct.compareTo : struct.compareToValue
-      let code = `switch (${compare}) {\n`
-      for (const key in struct.fields) {
-        code += indent(`case "${key}": return ` + callType(struct.fields[key]))+'\n'
-      }
-      if (struct.default) {
-        code += indent('default: return ' + callType(struct.type)) + '\n'
-      }
-      code += `}`
-      return wrapReadCode(code)
-    },
-    'option': (type) => {
-      let code = 'const {value} = utils.bool[0](buffer, offset)\n'
-      code += 'if (value) {\n'
-      code += '  const { value, size } = ' + callType(type) + '\n'
-      code += '  return { value, size: size + 1 }\n'
-      code += '}\n'
-      code += 'return {size: 1}'
-      return wrapReadCode(code)
-    },
-
-    // structures
-    'array': (array) => {
-      let code = ''
-      if (array.countType) {
-        code += 'const { value: count, size: countSize } = ' + callType(array.countType) + '\n'
-      } else if (array.count) {
-        code += 'const count = ' + array.count + '\n'
-        code += 'const countSize = 0\n'
-      } else {
-        throw new Error('Array must contain either count or countType')
-      }
-      code += 'const data = []\n'
-      code += 'let size = countSize'
-      code += 'for (let i=0 ; i<count ; i++) {\n'
-      code += '  const elem = ' + callType(array.type, 'size') + '\n'
-      code += '  data.push(elem.value)\n'
-      code += '  size += elem.size\n'
-      code += '}\n'
-      code += 'return { value: data, size }'
-      return wrapReadCode(code)
-    },
-    'count': (type) => {
-      throw new Error('count not supported, use array')
-    },
-    'container': (values) => {
-      let code = ''
-      let offsetExpr = 'offset'
-      let names = []
-      for (const i in values) {
-        const { type, name } = values[i]
-        code += `const { value: ${name}, size: ${name}Size } = ` + callType(type, offsetExpr) + '\n'
-        offsetExpr += ` + ${name}Size`
-        names.push(name)
-      }
-      const sizes = offsetExpr.split(' + ')
-      sizes.shift()
-      if (sizes.length === 0) sizes.push('0')
-      code += 'return {value: { ' + names.join(', ') + ' }, size: ' + sizes.join(' + ') + '}'
-      return wrapReadCode(code)
-    },
-
-    // utils
-    'pstring': (string) => {
-      let code = ''
-      if (string.countType) {
-        code += 'const { value: count, size: countSize } = ' + callType(string.countType) + '\n'
-      } else if (string.count) {
-        code += 'const count = ' + string.count + '\n'
-        code += 'const countSize = 0\n'
-      } else {
-        throw new Error('pstring must contain either count or countType')
-      }
-      code += 'offset += countSize\n'
-      code += 'if (offset + count > buffer.length) {\n'
-      code += '  throw new PartialReadError()\n'
-      code += '}\n'
-      code += 'return { value: buffer.toString(\'utf8\', offset, offset + count), size: count + countSize }'
-      return wrapReadCode(code)
-    },
-    'buffer': (buffer) => {
-      let code = ''
-      if (buffer.countType) {
-        code += 'const { value: count, size: countSize } = ' + callType(buffer.countType) + '\n'
-      } else if (buffer.count) {
-        code += 'const count = ' + buffer.count + '\n'
-        code += 'const countSize = 0\n'
-      } else {
-        throw new Error('buffer must contain either count or countType')
-      }
-      code += 'offset += countSize\n'
-      code += 'if (offset + count > buffer.length) {\n'
-      code += '  throw new PartialReadError()\n'
-      code += '}\n'
-      code += 'return { value: buffer.slice(offset, offset + count), size: count + countSize }'
-      return wrapReadCode(code)
-    },
-    'bitfield': (values) => {
-      let code = ''
-      let names = []
-      let totalSize = 8
-      code += 'let bits = buffer[offset++]\n'
-      for (const i in values) {
-        const { name, size, signed } = values[i]
-        while (totalSize < size) {
-          totalSize += 8
-          code += `bits = (bits << 8) | buffer[offset++]\n`
+class ReadCompiler {
+  constructor () {
+    this.primitiveTypes = {}
+    this.native = {}
+    this.context = {}
+    this.parameterizableTypes = {
+      // conditional
+      'switch': (compiler, struct) => {
+        const compare = struct.compareTo ? struct.compareTo : struct.compareToValue
+        let args = []
+        if (compare.startsWith('$')) args.push(compare)
+        let code = `switch (${compare}) {\n`
+        for (const key in struct.fields) {
+          code += indent(`case "${key}": return ` + compiler.callType(struct.fields[key])) + '\n'
         }
-        code += `let ${name} = (bits >> `+(totalSize - size)+') & 0x' + ((1 << size) - 1).toString(16) + '\n'
-        if (signed) code += `${name} -= (${name} & 0x` + (1 << (size - 1)).toString(16) + ') << 1\n'
-        totalSize -= size
-        names.push(name)
+        if (struct.default) {
+          code += indent('default: return ' + compiler.callType(struct.type)) + '\n'
+        }
+        code += `}`
+        return compiler.wrapCode(code, args)
+      },
+      'option': (compiler, type) => {
+        let code = 'const {value} = utils.bool[0](buffer, offset)\n'
+        code += 'if (value) {\n'
+        code += '  const { value, size } = ' + compiler.callType(type) + '\n'
+        code += '  return { value, size: size + 1 }\n'
+        code += '}\n'
+        code += 'return {size: 1}'
+        return compiler.wrapCode(code)
+      },
+
+      // structures
+      'array': (compiler, array) => {
+        let code = ''
+        if (array.countType) {
+          code += 'const { value: count, size: countSize } = ' + compiler.callType(array.countType) + '\n'
+        } else if (array.count) {
+          code += 'const count = ' + array.count + '\n'
+          code += 'const countSize = 0\n'
+        } else {
+          throw new Error('Array must contain either count or countType')
+        }
+        code += 'const data = []\n'
+        code += 'let size = countSize\n'
+        code += 'for (let i=0 ; i<count ; i++) {\n'
+        code += '  const elem = ' + compiler.callType(array.type, 'offset + size') + '\n'
+        code += '  data.push(elem.value)\n'
+        code += '  size += elem.size\n'
+        code += '}\n'
+        code += 'return { value: data, size }'
+        return compiler.wrapCode(code)
+      },
+      'count': (compiler, type) => {
+        throw new Error('count not supported, use array')
+      },
+      'container': (compiler, values) => {
+        let code = ''
+        let offsetExpr = 'offset'
+        let names = []
+        for (const i in values) {
+          const { type, name } = values[i]
+          code += `const { value: ${name}, size: ${name}Size } = ` + compiler.callType(type, offsetExpr) + '\n'
+          offsetExpr += ` + ${name}Size`
+          names.push(name)
+        }
+        const sizes = offsetExpr.split(' + ')
+        sizes.shift()
+        if (sizes.length === 0) sizes.push('0')
+        code += 'return { value: { ' + names.join(', ') + ' }, size: ' + sizes.join(' + ') + '}'
+        return compiler.wrapCode(code)
+      },
+
+      // utils
+      'pstring': (compiler, string) => {
+        let code = ''
+        if (string.countType) {
+          code += 'const { value: count, size: countSize } = ' + compiler.callType(string.countType) + '\n'
+        } else if (string.count) {
+          code += 'const count = ' + string.count + '\n'
+          code += 'const countSize = 0\n'
+        } else {
+          throw new Error('pstring must contain either count or countType')
+        }
+        code += 'offset += countSize\n'
+        code += 'if (offset + count > buffer.length) {\n'
+        code += '  throw new Error("Partial read at " + offset + " count: " + count)\n'
+        code += '}\n'
+        code += 'return { value: buffer.toString(\'utf8\', offset, offset + count), size: count + countSize }'
+        return compiler.wrapCode(code)
+      },
+      'buffer': (compiler, buffer) => {
+        let code = ''
+        if (buffer.countType) {
+          code += 'const { value: count, size: countSize } = ' + compiler.callType(buffer.countType) + '\n'
+        } else if (buffer.count) {
+          code += 'const count = ' + buffer.count + '\n'
+          code += 'const countSize = 0\n'
+        } else {
+          throw new Error('buffer must contain either count or countType')
+        }
+        code += 'offset += countSize\n'
+        code += 'if (offset + count > buffer.length) {\n'
+        code += '  throw new Error("Partial read at " + offset + " count: " + count)\n'
+        code += '}\n'
+        code += 'return { value: buffer.slice(offset, offset + count), size: count + countSize }'
+        return compiler.wrapCode(code)
+      },
+      'bitfield': (compiler, values) => {
+        let code = ''
+        let names = []
+        let totalSize = 8
+        code += 'let bits = buffer[offset++]\n'
+        for (const i in values) {
+          const { name, size, signed } = values[i]
+          while (totalSize < size) {
+            totalSize += 8
+            code += `bits = (bits << 8) | buffer[offset++]\n`
+          }
+          code += `let ${name} = (bits >> ` + (totalSize - size) + ') & 0x' + ((1 << size) - 1).toString(16) + '\n'
+          if (signed) code += `${name} -= (${name} & 0x` + (1 << (size - 1)).toString(16) + ') << 1\n'
+          totalSize -= size
+          names.push(name)
+        }
+        code += 'return { value: { ' + names.join(', ') + ' }, size: offset }'
+        return compiler.wrapCode(code)
+      },
+      'mapper': (compiler, mapper) => {
+        let code = 'const { value, size } = ' + compiler.callType(mapper.type) + '\n'
+        code += 'return { value: ' + JSON.stringify(mapper.mappings) + '[value], size }'
+        return compiler.wrapCode(code)
       }
-      code += 'return { value: { ' + names.join(', ') + ' }, size: offset }'
-      return wrapReadCode(code)
-    },
-    'mapper': (mapper) => {
-      let code = 'const { value, size } = ' + callType(mapper.type) + '\n'
-      code += 'return { value: ' + JSON.stringify(mapper.mappings) + '[value], size }'
-      return wrapReadCode(code)
+    }
+
+    // Add default types
+    for (const key in numeric) {
+      this.addNativeType(key, numeric[key][0])
+    }
+    for (const key in utils) {
+      this.addNativeType(key, utils[key][0])
     }
   }
 
-  function wrapReadCode(code) {
+  /**
+   * A native type is a type read or written by a function that will be called in it's
+   * original context.
+   * @param {*} type
+   */
+  addNativeType (type, fn) {
+    this.primitiveTypes[type] = `native.${type}`
+    this.native[type] = fn
+  }
+
+  /**
+   * A context type is a type that will be called in the protocol's context. It can refer to
+   * registred native types using native.{type}() or context type (provided and generated)
+   * using ctx.{type}(), but cannot access it's original context.
+   * @param {*} type
+   */
+  addContextType (type, fn) {
+    this.primitiveTypes[type] = `ctx.${type}`
+    this.context[type] = fn.toString()
+  }
+
+  /**
+   * A parametrizable type is a function that will be generated at compile time using the
+   * provided maker function
+   * @param {*} type
+   */
+  addParametrizableType (type, maker) {
+    this.parameterizableTypes[type] = maker
+  }
+
+  wrapCode (code, args = []) {
+    if (args.length > 0) return '(buffer, offset, ' + args.join(', ') + ') => {\n' + indent(code) + '\n}'
     return '(buffer, offset) => {\n' + indent(code) + '\n}'
   }
 
-  function callType (type, offsetExpr = 'offset') {
-    return '(' + compileType(type) + `)(buffer, ${offsetExpr})`
+  callType (type, offsetExpr = 'offset', args = []) {
+    if (args.length > 0) return '(' + this.compileType(type) + `)(buffer, ${offsetExpr}, ` + args.join(', ') + ')'
+    return '(' + this.compileType(type) + `)(buffer, ${offsetExpr})`
   }
 
-  function compileType (type) {
+  compileType (type) {
     if (type instanceof Array) {
-      if (parameterizableTypes[type[0]])
-        return parameterizableTypes[type[0]](type[1])
+      if (this.parameterizableTypes[type[0]]) { return this.parameterizableTypes[type[0]](this, type[1]) }
+      if (this.types[type[0]] && this.types[type[0]] !== 'native') {
+        return this.wrapCode('return ' + this.callType(type[0], 'offset', Object.values(type[1])))
+      }
       throw new Error('Unknown parametrizable type: ' + type[0])
     } else { // Primitive type
-      if (types[type] && types[type] !== 'native')
-        return compileType(types[type])
-      return primitiveTypes[type][0]
+      if (type === 'native') return 'null'
+      if (this.types[type] && this.types[type] !== 'native') { return 'ctx.' + type }
+      return this.primitiveTypes[type]
     }
   }
 
-  return compileType(types[mainType])
+  generate (types) {
+    let functions = []
+    this.types = types
+    for (const type in this.context) {
+      functions.push(`${type}: ` + this.context[type])
+    }
+    for (const type in types) {
+      if (types[type] !== 'native') { functions.push(`${type}: ` + this.compileType(types[type])) }
+    }
+    return '() => {\nconst ctx = {\n' + indent(functions.join(',\n')) + '\n}\n  return ctx\n}'
+  }
+
+  /**
+   * Compile the given js code, providing native.{type} to the context, return the compiled types
+   * @param {*} code
+   */
+  compile (code) {
+    // Local variable to provide some context to eval()
+    const native = this.native // eslint-disable-line
+    return eval(code)() // eslint-disable-line
+  }
 }
 
-function optimize(code, cb) {
+function optimize (code, cb) {
   const closureCompiler = new ClosureCompiler({
     compilation_level: 'SIMPLE'
   })
@@ -181,12 +237,7 @@ function optimize(code, cb) {
   })
 }
 
-function compile(code) {
-  return eval(code)
-}
-
 module.exports = {
-  compile,
-  optimize,
-  generateRead
+  ReadCompiler,
+  optimize
 }
