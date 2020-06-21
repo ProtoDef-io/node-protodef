8753adf4a7b5464c06183daf72af3c2cc07b9cc7
Romain Beaumont <romain.rom1@gmail.com>
2020-06-21 18:44:32 +0200

    add option not to log errors in FullPacketParser

--- src/serializer.js
+++ src/serializer.js
@@ -57,10 +57,11 @@ class Parser extends Transform {
 }
 
 class FullPacketParser extends Transform {
-  constructor (proto, mainType) {
+  constructor (proto, mainType, noErrorLogging = false) {
     super({ readableObjectMode: true })
     this.proto = proto
     this.mainType = mainType
+    this.noErrorLogging = noErrorLogging
   }
 
   parsePacketBuffer (buffer) {
@@ -71,7 +72,7 @@ class FullPacketParser extends Transform {
     let packet
     try {
       packet = this.parsePacketBuffer(chunk)
-      if (packet.metadata.size !== chunk.length) {
+      if (packet.metadata.size !== chunk.length && !this.noErrorLogging) {
         console.log('Chunk size is ' + chunk.length + ' but only ' + packet.metadata.size + ' was read ; partial packet : ' +
           JSON.stringify(packet.data) + '; buffer :' + chunk.toString('hex'))
       }
